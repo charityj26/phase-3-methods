3a3c292a0f510d870900cd7fdaa41e18a60ec270
charity <charity.joseph@students.moringaschool.com>
2023-05-15 15:19:49 +0300

    completed work

--- methods.rb
+++ methods.rb
@@ -1,1 +1,23 @@
 # Your code here!
+def greet_programmer()
+puts "Hello,programmer!"
+end
+
+def greet (name)
+puts "Hello,#{name}!"
+end
+
+def greet_with_default(name="programmer")
+puts "Hello,#{name}!"
+end
+
+def add (val1,val2)
+return val1 +val2
+end
+
+def halve (num)
+if num.class !=integer
+return nil
+end
+num/2
+end
